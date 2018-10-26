9bf6a7695105b10c21dab663ba30c94de416b5b3
Lukas Weber <devlukas@hotmail.com>
2018-10-26 14:40:08 +0200

    Add base layout

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -3,7 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
