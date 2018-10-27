3f9ead65e6a697a7569d3a2bfc514685cef34edc
Jonas Wyss <Jonas.Wyss@landisgyr.com>
2018-10-27 02:22:12 +0200

    fix?

--- src/server/ticker/repostiory.ts
+++ src/server/ticker/repostiory.ts
@@ -4,7 +4,7 @@ import { Player } from './model/player';
 import { Match } from './model/match';
 import { Ranking } from './model/ranking';
 import { Arena } from './model/arena';
-import {Partie} from "./model/partie";
+import { Partie } from './model/partie';
 @Injectable()
 export class Repository {
     constructor(private readonly db: DbService) { }
@@ -22,20 +22,20 @@ export class Repository {
         // return await this.db.k('player').where(team == teamuid);
     }
 
-    public async findMatch(uid: string): Promise < Match > {
+    public async findMatch(uid: string): Promise<Match> {
         return await this.db.k('match').first().where({ uid });
     }
 
-    public async getMatches(): Promise < Player[] > {
-    return await this.db.k('match');
-}
+    public async getMatches(): Promise<Player[]> {
+        return await this.db.k('match');
+    }
 
-    public async findPartie(name: string): Promise<Partie[]>  {
-        return await this.db.k('partie').where('homename', name).orWhere('guestname',name);
+    public async findPartie(name: string): Promise<Partie[]> {
+        return await this.db.k('partie').where('homename', name).orWhere('guestname', name);
 
     }
 
-    public async getRankings(): Promise<Ranking[]>  {
+    public async getRankings(): Promise<Ranking[]> {
         return await this.db.k('ranking');
     }
 
@@ -43,11 +43,14 @@ export class Repository {
         return await this.db.k('arena');
     }
 
+    public async findRanking(uid: string): Promise<Ranking> {
+        return await this.db.k('ranking').first().where({ uid });
+    }
+
+    public async findArena(uid: string): Promise<Arena> {
+        return await this.db.k('arena').first().where({ uid });
+    }
 }
 
-    public async findArena(uid: string): Promise < Arena > {
-    return await this.db.k('arena').first().where({ uid });
-}
-}
 
 //new Repository(new DbService()).findMatch('040A36A7-5E75-49E8-B2D9-0F3790572EA4');
